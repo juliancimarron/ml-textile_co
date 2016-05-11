ea17db1468b54dd89533c9cd3746d69fe96ce3bd
Julian Hernandez <julian1@cimarron.me>
2016-05-11 02:15:48 -0500

    Devise generate for Employee created and migrated. Employee#index made site root.

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  root 'employees#index'
+
+  devise_for :employees
+
   resources :employees
   resources :departments
-  
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160511070501) do
+ActiveRecord::Schema.define(version: 20160511071217) do
 
   create_table "departments", force: :cascade do |t|
     t.string   "name"
@@ -25,10 +25,22 @@ ActiveRecord::Schema.define(version: 20160511070501) do
     t.string   "last_name"
     t.integer  "department_id"
     t.boolean  "admin"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
 
   add_index "employees", ["department_id"], name: "index_employees_on_department_id"
+  add_index "employees", ["email"], name: "index_employees_on_email", unique: true
+  add_index "employees", ["reset_password_token"], name: "index_employees_on_reset_password_token", unique: true
 
 end
