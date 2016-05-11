5c4af2a346f70f8902c85c3ec5003095f75fe08a
Julian Hernandez <julian1@cimarron.me>
2016-05-11 03:45:18 -0500

    belongs_to statement in Department model moved

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,7 +1,8 @@
 class Employee < ActiveRecord::Base
+  belongs_to :department
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  belongs_to :department
 end
