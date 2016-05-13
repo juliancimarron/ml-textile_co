546c64bebead1e50dcf4b563f99759b34ff144ae
Julian Hernandez <julian1@cimarron.me>
2016-05-13 05:20:12 -0500

    Timesheet model reorganized

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -38,30 +38,33 @@ class Timesheet < ActiveRecord::Base
     :validate_pay_date
   )
 
-  def employee_ids
-    Employee.all.pluck(:employee_id)
-  end
-
-  def validate_period_end_date 
-    return if period_end_date.nil? or period_start_date.nil?
-
-    if period_end_date < period_start_date
-      errors.add(:period_end_date, "The Period End Date cannot happen before the Period Start Date.")
-    end
-  end
-
-  def validate_pay_date
-    return if pay_date.nil? or period_end_date.nil?
-
-    if pay_date < period_end_date
-      errors.add(:pay_date, "The Pay Date cannot happen before the Period End Date.")
-    end
-  end
 
   def self.print_timesheet_period(record) 
     record.period_start_date.strftime('%d/%b/%Y') +
     ' –– ' +
     record.period_end_date.strftime('%d/%b/%Y')
   end
+
+  private
+
+    def employee_ids
+      Employee.all.pluck(:employee_id)
+    end
+
+    def validate_period_end_date 
+      return if period_end_date.nil? or period_start_date.nil?
+
+      if period_end_date < period_start_date
+        errors.add(:period_end_date, "The Period End Date cannot happen before the Period Start Date.")
+      end
+    end
+
+    def validate_pay_date
+      return if pay_date.nil? or period_end_date.nil?
+
+      if pay_date < period_end_date
+        errors.add(:pay_date, "The Pay Date cannot happen before the Period End Date.")
+      end
+    end
 
 end
