4d7b90a065bd4af0f0262deddbf4bf87d77c0d10
Julian Hernandez <julian1@cimarron.me>
2016-05-13 05:54:49 -0500

    timelogs#edit view created and tested

--- spec/views/timelogs/edit.html.haml_spec.rb
+++ spec/views/timelogs/edit.html.haml_spec.rb
@@ -1,21 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "timelogs/edit", type: :view do
+
+  fixtures :timelogs
+  fixtures :timesheets
+
   before(:each) do
-    @timelog = assign(:timelog, Timelog.create!(
-      :employee => nil,
-      :claim_status => "MyString"
-    ))
+    @timelog = timelogs(:john)
+    @timesheet = timesheets(:john)
   end
 
-  it "renders the edit timelog form" do
-    render
-
-    assert_select "form[action=?][method=?]", timelog_path(@timelog), "post" do
-
-      assert_select "input#timelog_employee_id[name=?]", "timelog[employee_id]"
-
-      assert_select "input#timelog_claim_status[name=?]", "timelog[claim_status]"
-    end
+  describe 'form to enter claim data' do
+    specify { expect(render).to have_css '.top-bar', count: 1 }
+    specify { expect(render).to have_text Timesheet.print_timesheet_period(@timesheet) }
+    specify { expect(render).to have_css 'form input[type="submit"]', count: 1 }
+    specify { expect(render).to have_text @timelog.arrive_datetime.strftime '%l:%M %p' }
+    specify { text = @timelog.claim_arrive_datetime.strftime('%l:%M %p')
+      expect(render).to have_css "input[type='text'][value='#{text}']"}
   end
+
 end
