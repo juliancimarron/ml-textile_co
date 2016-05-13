b650a039f5969fc0ead4a3ab7a4e0bd30950aae2
Julian Hernandez <julian1@cimarron.me>
2016-05-13 18:51:52 -0500

    department and employee factories created

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,2 +1,14 @@
 FactoryGirl.define do
+
+  factory :department do
+    name 'Development'
+  end
+
+  factory :employee do
+    first_name FFaker::Name.first_name
+    last_name FFaker::Name.first_name
+    association :department
+    trait(:admin) { admin true }
+  end
+
 end
