e79e6b400fb50f632ecb3de97a25279550f2b348
Julian Hernandez <julian1@cimarron.me>
2016-05-13 18:56:21 -0500

    timelogs#index tests cleaned up using shared_context

--- spec/views/timelogs/index.html.haml_spec.rb
+++ spec/views/timelogs/index.html.haml_spec.rb
@@ -1,37 +1,22 @@
 require 'rails_helper'
+require 'support/shared_context'
 
 RSpec.describe "timelogs/index", type: :view do
+  include_context 'create_timelogs_timesheets'
 
   fixtures :employees
 
   describe "renders the signed in employee's timelogs" do
-    let(:last_month_day) { 31 }
     let(:month) { 5 }
     let(:year) { 2016 }
+    let(:workdays) { month_workdays(year, month) }
     let(:employee) { employees(:john) }
 
     before(:example) do
       sign_in employee
 
-      base_dt = DateTime.new year, month
-      timelog_dt = base_dt
-
-      timelog_dt = base_dt - 1.day
-      (last_month_day - 1).times {
-        timelog_dt += 1.day
-        Timelog.create(
-          employee_id: employee.id,
-          log_date: timelog_dt.to_date,
-          arrive_datetime: (timelog_dt + 9.hours),
-          leave_datetime: (timelog_dt + 18.hours)
-        )
-      }
-
-      Timesheet.create( 
-        employee_id: employee.id,
-        period_start_date: base_dt.to_date,
-        period_end_date: (base_dt + last_month_day.days - 1.days).to_date
-      )
+      create_timelogs(Date.new(2016,3,1), Date.new(2016,5,31), employee)
+      create_timesheets(Date.new(2016,3,1), Date.new(2016,5,31), employee)
 
       @timelogs = Timelog.all
       @timesheets = Timesheet.all
@@ -42,7 +27,7 @@ RSpec.describe "timelogs/index", type: :view do
 
     specify { expect(render).to have_css '.top-bar', count: 1 }
     specify { expect(render).to have_css 'form', count: 1 }
-    specify { expect(render).to have_css 'tbody > tr', count: last_month_day }
+    specify { expect(render).to have_css 'tbody > tr', count: workdays }
     specify { expect(render).to have_css 'th', text: 'Minutes' }
     specify { expect(render).to have_text @timelogs.sample[:leave] }
   end
