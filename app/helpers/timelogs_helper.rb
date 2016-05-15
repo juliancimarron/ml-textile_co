550fee2a3bac1292b84b7719ca58381515393ace
Julian Hernandez <julian1@cimarron.me>
2016-05-15 00:26:14 -0500

    TimelogsHelper#print_late_by added to print lateness in minutes

--- app/helpers/timelogs_helper.rb
+++ app/helpers/timelogs_helper.rb
@@ -1,2 +1,15 @@
 module TimelogsHelper
+
+  def print_late_by(arrive_time, timelog) 
+    correct_arrive = timelog.log_date + arrive_time
+    res = 'N/A'
+
+    if arrive = Timelog.get_correct_moment(:arrive, timelog)
+      sec_diff = arrive ? (arrive - correct_arrive).to_i : 0
+      res = sec_diff > 0 ? "#{sec_diff / 60} minutes" : nil
+    end
+
+    return res
+  end
+
 end
--- spec/helpers/timelogs_helper_spec.rb
+++ spec/helpers/timelogs_helper_spec.rb
@@ -1,15 +1,54 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the TimelogsHelper. For example:
-#
-# describe TimelogsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe TimelogsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  fixtures :employees
+
+  describe '#print_late_by' do
+    let(:today) { DateTime.new 2016,5,1 }
+    let(:scheduled_arrive_time) { 9.hours }
+
+    it "returns time difference if arrive_datetime > scheduled" do
+      late_by = 35.minutes
+      timelog = Timelog.new(
+        employee: employees(:john),
+        log_date: today,
+        arrive_datetime: today + scheduled_arrive_time + late_by
+      )
+      expect(print_late_by(scheduled_arrive_time, timelog)).to eq "#{late_by / 60} minutes"
+    end
+
+    it "returns time difference if claim_arrive_datetime > scheduled and claim_status = pending" do
+      late_by = 35.minutes
+      timelog = Timelog.new(
+        employee: employees(:john),
+        log_date: today,
+        claim_arrive_datetime: (today + scheduled_arrive_time + late_by),
+        claim_status: 'pending'
+      )
+      expect(print_late_by(scheduled_arrive_time, timelog)).to eq "#{late_by / 60} minutes"
+    end
+
+    it "returns time difference if arrive_datetime > scheduled and claim_status = declined" do
+      late_by = 35.minutes
+      timelog = Timelog.new(
+        employee: employees(:john),
+        log_date: today,
+        arrive_datetime: (today + scheduled_arrive_time + late_by),
+        claim_arrive_datetime: (today + scheduled_arrive_time + late_by + 2.hours),
+        claim_status: 'declined'
+      )
+      expect(print_late_by(scheduled_arrive_time, timelog)).to eq "#{late_by / 60} minutes"
+    end
+
+    it "returns 'N/A' if arrive_datetime.nil? and claim_arrive_datetime.nil?" do
+      late_by = 35.minutes
+      timelog = Timelog.new(
+        employee: employees(:john),
+        log_date: today,
+      )
+      expect(print_late_by(scheduled_arrive_time, timelog)).to eq "N/A"
+    end
+  end
+
 end
