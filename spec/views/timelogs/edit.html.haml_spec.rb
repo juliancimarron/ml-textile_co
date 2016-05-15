d943b2974d578c5ac7e60d1bc486102dffece7aa
Julian Hernandez <julian1@cimarron.me>
2016-05-15 05:54:38 -0500

    timelogs#edit view updated to work with arrive and leave as integers

--- spec/views/timelogs/edit.html.haml_spec.rb
+++ spec/views/timelogs/edit.html.haml_spec.rb
@@ -4,8 +4,10 @@ RSpec.describe "timelogs/edit", type: :view do
 
   fixtures :timelogs
   fixtures :timesheets
+  fixtures :employees
 
   before(:each) do
+    sign_in employees(:john)
     @timelog = timelogs(:john)
     @timesheet = timesheets(:john)
   end
@@ -14,8 +16,8 @@ RSpec.describe "timelogs/edit", type: :view do
     specify { expect(render).to have_css '.top-bar', count: 1 }
     specify { expect(render).to have_text Timesheet.print_timesheet_period(@timesheet) }
     specify { expect(render).to have_css 'form input[type="submit"]', count: 1 }
-    specify { expect(render).to have_text @timelog.arrive_datetime.strftime '%l:%M %p' }
-    specify { text = @timelog.claim_arrive_datetime.strftime('%l:%M %p')
+    specify { expect(render).to have_text (@timelog.log_date + @timelog.arrive_sec).strftime '%l:%M %p' }
+    specify { text = (@timelog.log_date + @timelog.claim_arrive_sec).strftime('%l:%M %p')
       expect(render).to have_css "input[type='text'][value='#{text}']"}
   end
 
