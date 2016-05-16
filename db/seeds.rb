324d9cd9ccfb6c94301045f9bc530add58978070
Julian Hernandez <julian1@cimarron.me>
2016-05-16 00:07:39 -0500

    Seeds file updated with all new changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -71,72 +71,23 @@ ActiveRecord::Base.transaction do
     puts date += 1.day
     next if [0,6].include? date.wday
     Employee.all.each do |emp|
-      arrive = date.to_datetime + 9.hours + dist_rand[
-        [0, rand(1..30).minutes], [90, 100], true
-      ].seconds
-      leave = date.to_datetime + 18.hours + dist_rand[
-        [0, rand(1..30).minutes], [90, 100], true
-      ].seconds
-      claim_arrive = rand(0..99) > 95 ? arrive + dist_rand[[rand(30..60).minutes], [100], true].seconds : nil
-      claim_leave = rand(0..99) > 95 ? arrive + dist_rand[[rand(30..60).minutes], [100], true].seconds : nil
+      next if dist_rand[[false,true], [95,100], false]
+      arrive = 9.hours + dist_rand[ [0,rand(1..30).minutes],[90,100],true ].seconds
+      leave = 18.hours + dist_rand[ [0,rand(1..30).minutes],[90,100],true ].seconds
+      claim_arrive = rand(0..99) > 95 ? arrive + dist_rand[ [rand(30..60).minutes],[100],true ].seconds : nil
+      claim_leave = rand(0..99) > 95 ? arrive + dist_rand[ [rand(30..60).minutes],[100],true ].seconds : nil
       Timelog.create(
         employee_id: emp.employee_id,
         log_date: date,
-        arrive_datetime: arrive,
-        leave_datetime: leave,
-        claim_arrive_datetime: claim_arrive,
-        claim_leave_datetime: claim_leave,
+        arrive_sec: arrive,
+        leave_sec: leave,
+        claim_arrive_sec: claim_arrive,
+        claim_leave_sec: claim_leave,
         claim_status: (claim_arrive.nil? and claim_leave.nil?) ? nil : Timelog::CLAIM_STATUS_OPT.sample
       )
     end
   end
 end
-Timelog.all.select{|t| dist_rand[[false,true], [95,100], false]}
-  .each{|t| 
-    t.arrive_datetime = nil
-    t.leave_datetime = nil
-    t.claim_arrive_datetime = nil
-    t.claim_leave_datetime = nil
-    t.claim_status = nil
-    t.save
-  }
 puts "Finished seeding Timelog data."
 puts dotted
-
-
-# Timesheet
-puts "Now seeding Timesheet data."
-date_start = timelogs_start_date
-date_end = Date.new(date_start.year, date_start.month, -1)
-ActiveRecord::Base.transaction do
-  while date_start <= timelogs_end_date
-    puts date_start
-    Employee.all.each do |emp|
-      seconds = Timelog.where(
-        'employee_id = ? AND log_date >= ? AND log_date <= ?',
-        emp.id, date_start, date_end
-      ).map{|x| 
-        arrive = x.arrive_datetime
-        leave = x.leave_datetime
-        if ['pending','approved'].include? x.claim_status
-          arrive = x.claim_arrive_datetime unless x.claim_arrive_datetime.nil?
-          leave = x.claim_leave_datetime unless x.claim_leave_datetime.nil?
-        end
-        (leave.nil? or arrive.nil?) ? 0 : leave - arrive
-      }.inject(:+).to_i
-      Timesheet.create(
-        employee: emp,
-        period_start_date: date_start,
-        period_end_date: date_end,
-        logged_hrs: Util.seconds_to_hrs_min(seconds)[:hours],
-        logged_min: Util.seconds_to_hrs_min(seconds)[:minutes],
-        pay_date: date_end + 7.days
-      )
-    end
-    date_start = date_end + 1.day
-    date_end = Date.new(date_start.year, date_start.month, -1)
-  end
-end
-puts "Finished seeding Timesheet data"
-puts dotted
-puts "All seed data how now been added successfully."
+puts "Finished seeding all data."
