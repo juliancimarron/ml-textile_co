4b6d3c15ad025237623d8a4cfe1ad5fca75764a2
Julian Hernandez <julian1@cimarron.me>
2016-05-15 19:54:43 -0500

    timelogs#edit view updated to drop use of Timesheet

--- spec/views/timelogs/edit.html.haml_spec.rb
+++ spec/views/timelogs/edit.html.haml_spec.rb
@@ -3,21 +3,20 @@ require 'rails_helper'
 RSpec.describe "timelogs/edit", type: :view do
 
   fixtures :timelogs
-  fixtures :timesheets
   fixtures :employees
 
   before(:each) do
     sign_in employees(:john)
     @timelog = timelogs(:john)
-    @timesheet = timesheets(:john)
+    @period = 'This is the pay period'
   end
 
   describe 'form to enter claim data' do
     specify { expect(render).to have_css '.top-bar', count: 1 }
-    specify { expect(render).to have_text Timesheet.print_timesheet_period(@timesheet) }
+    specify { expect(render).to have_text @period }
     specify { expect(render).to have_css 'form input[type="submit"]', count: 1 }
-    specify { expect(render).to have_text (@timelog.log_date + @timelog.arrive_sec).strftime '%l:%M %p' }
-    specify { text = (@timelog.log_date + @timelog.claim_arrive_sec).strftime('%l:%M %p')
+    specify { expect(render).to have_text (DateTime.new(2016) + @timelog.arrive_sec.seconds).strftime '%l:%M %p' }
+    specify { text = (DateTime.new(2016) + @timelog.claim_arrive_sec.seconds).strftime('%l:%M %p')
       expect(render).to have_css "input[type='text'][value='#{text}']"}
   end
 
